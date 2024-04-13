988317308595a517a260f15cdbda0c60e4b623f9
Sanidhya Pundeer <pundeer.sanidhya@gmail.com>
2024-04-14 00:53:27 +0530

    provider implementation in process

--- shopping_cart/lib/main.dart
+++ shopping_cart/lib/main.dart
@@ -13,11 +13,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: Text('Shopping Cart'),
         ),
+        body: ProductList(),
       ),
     );
+  }
+}
+
+class ProductList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
   }
 }
--- shopping_cart/lib/model/cartModel.dart
+++ shopping_cart/lib/model/cartModel.dart
@@ -1,5 +1,3 @@
-import 'dart:js_interop';
-
 import 'package:flutter/material.dart';
 import 'package:shopping_cart/model/products.dart';
 
